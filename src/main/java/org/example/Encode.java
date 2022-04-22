d48440e3b95bb30b035a1a6b7da2e57185697829
Tess <njerigitundu003@gmail.com>
2022-04-22 22:41:36 +0300

    Create a platform to collect data from the user

--- src/main/java/org/example/Encode.java
+++ src/main/java/org/example/Encode.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Encode {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter any text you'd want us to encode:");
+        String userText = sc.nextLine();
+        System.out.println("Your text is: " + userText );
+    }
 }
--- src/test/java/org/example/EncodeTest.java
+++ src/test/java/org/example/EncodeTest.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.junit.*;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncodeTest {
