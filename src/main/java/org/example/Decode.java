d30c95dece2f76c34d09c70b04ada77fd43b2ed1
Tess <njerigitundu003@gmail.com>
2022-04-23 16:17:36 +0300

    Add function for decoding user input

--- src/main/java/org/example/Decode.java
+++ src/main/java/org/example/Decode.java
@@ -1,23 +1,23 @@
 package org.example;
 
 public class Decode {
-//    private char[] decoder =  new char[26];
-//    protected String userPlainText;
-//
-//    public Decode(int rotation) {
-//
-//        for (int k = 0; k < 26; k++) {
-//
-//                decoder[k] = (char) ('A' + (k - rotation + 26) % 26);
-//            }
-//
-//        }
-//
-//
-//    public String getUserPlainTextText(String userPlainText) {
-//        return shifted(userPlainText, decoder);
-//    }
-//
+    private char[] decoder =  new char[26];
+    protected String userPlainText;
+
+    public Decode(int rotation) {
+
+        for (int k = 0; k < 26; k++) {
+
+                decoder[k] = (char) ('A' + (k - rotation + 26) % 26);
+            }
+
+        }
+
+
+    public String getUserPlainTextText(String userPlainText) {
+        return shifted(userPlainText, decoder);
+    }
+
 //    public String shifted(String original, char[] code) {
 //        char[] text = original.toCharArray();
 //        for (int k = 0; k < text.length; k++)
@@ -26,5 +26,5 @@ public class Decode {
 //                text[k] = code[j];
 //            }
 //        return new String(text);
-//    }
-//}
+    }
+}
