9f1cc8a5acecb18a3cb083ce64b0bc587461ff73
Tess <njerigitundu003@gmail.com>
2022-04-23 16:19:14 +0300

    Create getters and setters for decoding

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,12 +7,12 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         Encode encode = new Encode(3);
-//        Decode decode = new Decode(3);
+        Decode decode = new Decode(3);
         System.out.println("Enter text you'd like encoded here. ONLY USE LETTERS OF THE ALPHABET");
         String userInputtedText = sc.nextLine();
         String codedText = encode.getUserText(userInputtedText);
         System.out.println("Text to be encoded: " + codedText);
-//        String answer = decode.getUserPlainTextText(codedText);
-//        System.out.println("Your encoded message is: " + answer);
+        String answer = decode.getUserPlainTextText(codedText);
+        System.out.println("Your encoded message is: " + answer);
     }
 }
