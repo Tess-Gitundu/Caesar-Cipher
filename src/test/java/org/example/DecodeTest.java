d29861c73f5988e5accfec7420e142c64ba92d44
Tess <njerigitundu003@gmail.com>
2022-04-23 16:51:33 +0300

    Create another test to check if shifting is working

--- src/test/java/org/example/DecodeTest.java
+++ src/test/java/org/example/DecodeTest.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecodeTest {
+    @Test
+    public void userInput_checkIfInputString_true() {
 
+        Encode userInputTest = new Encode(3);
+        assertEquals(true, userInputTest instanceof Encode);
+    }
+    @Test
+    public void userInput_checkIfInputShifting_booleanTrue() {
+
+        Encode userInputTest = new Encode(3);
+        assertEquals(true, userInputTest instanceof Encode);
+    }
 }
